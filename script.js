0fe392e1a2421ce27a2dcab5425c586be0f8ab12
Tom West <seriousbsnz@gmail.com>
2025-05-07 17:49:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ import * as THREE from 'https://unpkg.com/three@0.150.0/build/three.module.js';
 
 // Create scene, camera, renderer
 const scene    = new THREE.Scene();
-
 const camera   = new THREE.PerspectiveCamera( 50, window.innerWidth/window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
